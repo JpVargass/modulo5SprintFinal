0f44b84c0986cddc18fc91cda9e94f24265ebcbf
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-23 10:09:07 -0400

    Initial commit

---
+++ app/src/main/java/cl/jpvs/modulo5sprintfinal/MainActivity.kt
@@ -0,0 +1,15 @@
+package cl.jpvs.modulo5sprintfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+*   Shoe Tap
+*  Primera Pantalla (Activitys)
+* ( ) crear panta inicio que muestre lista zapatos
+* */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
