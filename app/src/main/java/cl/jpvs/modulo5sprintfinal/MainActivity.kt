29c8ecb2fd33a7d02fd26b9461c682a62970f2b8
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-24 04:15:02 -0400

    Creación Class data zapatillasTienda

--- app/src/main/java/cl/jpvs/modulo5sprintfinal/MainActivity.kt
+++ app/src/main/java/cl/jpvs/modulo5sprintfinal/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 /*
 *   Shoe Tap
 *  Primera Pantalla (Activitys)
-* ( ) crear panta inicio que muestre lista zapatos
+* ( ) crear pantalla inicio que muestre lista zapatos
+* ( ) Utilizar RecyclerView para mostrar en foma de lista debe incluir imagen y precio
+* ( ) Puedes implementar esta pantalla utilizando fragmento en Android Studio
+* ( ) los productos deben ser una lista estática local
+*
 * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
