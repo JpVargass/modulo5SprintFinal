47643b8429b940276e0c9544f992c994107263ea
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-31 01:44:11 -0400

    mejoras en la visualizacion

--- app/src/main/java/cl/jpvs/modulo5sprintfinal/zapatillasTienda.kt
+++ app/src/main/java/cl/jpvs/modulo5sprintfinal/zapatillasTienda.kt
@@ -8,91 +8,84 @@ class zapatillasTienda {
             zapatillasList.add(
                 Zapatilla(
                     "Rebound joy low zapatilla urbana hombre azul Puma",
-                    "https://falabella.scene7.com/is/image/Falabella/50196585_1?wid=800&hei=800&qlt=70",
-                    32990.0
-                )
-            )
-            zapatillasList.add(
-                Zapatilla(
-                    "Rebound joy low zapatilla urbana hombre azul Puma",
-                    "https://falabella.scene7.com/is/image/Falabella/50196585_1?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/50196585_1?wid=400&hei=400&qlt=70",
                     32990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Zapatilla Urbana Hombre Negro Americanino",
-                    "https://falabella.scene7.com/is/image/Falabella/882678371_4?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/882678371_4?wid=400&hei=400&qlt=70",
                     14990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Zapatilla Urbana Hombre Negro Newport",
-                    "https://falabella.scene7.com/is/image/Falabella/882678362_3?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/882678362_3?wid=400&hei=400&qlt=70",
                     11990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Zapatilla Urbana Hombre Azul Newport",
-                    "https://falabella.scene7.com/is/image/Falabella/882678305_4?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/882678305_4?wid=400&hei=400&qlt=70",
                     11990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Zapatilla Hombre Predict Soc3 Negro Salomon",
-                    "https://falabella.scene7.com/is/image/Falabella/gsc_118344932_2065763_1?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/gsc_118344932_2065763_1?wid=400&hei=400&qlt=70",
                     71990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Zapatilla Hombre Outrise Gris Salomon",
-                    "https://falabella.scene7.com/is/image/Falabella/gsc_118374745_2065727_1?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/gsc_118374745_2065727_1?wid=400&hei=400&qlt=70",
                     71990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Crater Remixa Zapatilla Urbana Hombre Negro Nike",
-                    "https://falabella.scene7.com/is/image/Falabella/15313536_01?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/15313536_01?wid=400&hei=400&qlt=70",
                     34990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Run 60S 3.0 Zapatilla Urbana Hombre Azul Adidas",
-                    "https://falabella.scene7.com/is/image/Falabella/50182928_3?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/50182928_3?wid=400&hei=400&qlt=70",
                     59990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Puma Zapatilla urbana hombre blanco",
-                    "https://falabella.scene7.com/is/image/Falabella/15335576_1?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/15335576_1?wid=400&hei=400&qlt=70",
                     54990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Puma Zapatilla Puma Rebound Joy Multicolor Zapatilla Urbana Hombre Azul",
-                    "https://falabella.scene7.com/is/image/Falabella/50251171_1?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/50251171_1?wid=400&hei=400&qlt=70",
                     59990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Mountain gear Plomozapatilla outdoor hombre negro",
-                    "https://falabella.scene7.com/is/image/Falabella/882430309_4?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/882430309_4?wid=400&hei=400&qlt=70",
                     19990.0
                 )
             )
             zapatillasList.add(
                 Zapatilla(
                     "Gel-Quantum 90 Zapatilla Urbana Hombre Negra Asics Asics",
-                    "https://falabella.scene7.com/is/image/Falabella/50168978_1?wid=800&hei=800&qlt=70",
+                    "https://falabella.scene7.com/is/image/Falabella/50168978_1?wid=400&hei=400&qlt=70",
                     69990.0
                 )
             )
